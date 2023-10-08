bda18b36bf98dacddb21f71d1f3860e14d59f24a
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-07 22:07:54 -0500

    Commit Initial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'; 
+
+const app = express()
+app.use(express.json()) 
+
+const PORT = 3000
+
+app.get("/", (_req, res) => {
+    console.log("Hola")
+    const message = "Hola mundo"
+    res.send(message)
+})
+ 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
