9694e1f9c50081d2966986a7c7170cf9c97d5de0
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-07 22:40:15 -0500

    Add lint

--- src/api/v1/controllers/login.controllers.ts
+++ src/api/v1/controllers/login.controllers.ts
@@ -1,7 +1,11 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
 
-export function loginGet(_req: Request, res: Response, _next: NextFunction) {
-    res.send({ data: "Estos son los datos de respuesta del login" })
+export function loginGet(_req: Request, res: Response, next: NextFunction) {
+  try {
+    res.send({ data: "Estos son los datos de respuesta del login" });
+  } catch (error) {
+    next(error);
+  }
 }
 
 // export async function login(_req: Request, res: Response, next: NextFunction) {
@@ -12,4 +16,4 @@ export function loginGet(_req: Request, res: Response, _next: NextFunction) {
 //     }
 
 //     return true
-// } 
+// }
