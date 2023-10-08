300737e12715cf71329730452f3226d55ab4245e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-07 22:51:27 -0500

    Fix with eslint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import loginRouter from "./v1/routes/login.router"
+import loginRouter from "./v1/routes/login.router";
 
 function routerApi(app: express.Application) {
-    const router = express.Router()
-    app.use("/api/v1", router)
-    router.use("/login", loginRouter)
+  const router = express.Router();
+  app.use("/api/v1", router);
+  router.use("/login", loginRouter);
 }
-
 
 export default routerApi;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import "dotenv/config"
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import cors from "cors";
 import routerApi from "./api";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-    console.log("Hola")
-    const message = "Hola mundo"
-    res.send(message)
-})
+  console.log("Hola");
+  const message = "Hola mundo";
+  res.send(message);
+});
 
-routerApi(app)
+routerApi(app);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
