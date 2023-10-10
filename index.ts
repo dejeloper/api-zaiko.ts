cb6d3c9314d89d38d1bb54f3f9306b9e902de196
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-09 23:36:31 -0500

    Update application's entry point (app and Index)

--- index.ts
+++ index.ts
@@ -1,16 +1,11 @@
 import "dotenv/config";
-import express from "express";
-import cors from "cors";
+import app from "./app";
 import routerApi from "./src/api";
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  const message = "Api Rest with TypeScript and Express - Api Zaiko";
+  const message = "Api Zaiko - Typescript and express";
   res.send(message);
 });
 
